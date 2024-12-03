c68fa34e0a0e37c883dcf135e1b564d3c5af5d18
Ayush <112577569+Ayus444@users.noreply.github.com>
2024-12-04 00:09:14 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,10 @@
-api_id = ""
-api_hash = ""
-bot_token = ""
+import os
+
+# Fetch API credentials from environment variables
+api_id = os.getenv('TELEGRAM_API_ID')
+api_hash = os.getenv('TELEGRAM_API_HASH')
+bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
+
+# Ensure that the environment variables are set correctly
+#if not api_id or not api_hash or not bot_token:
+    #raise ValueError("Missing required environment variables: TELEGRAM_API_ID, TELEGRAM_API_HASH, or TELEGRAM_BOT_TOKEN")
